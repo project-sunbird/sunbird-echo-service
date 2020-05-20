6b5122431f32eef97c3c69725890496abde99581
Mathew Jose Pallan <mathewjpallan@gmail.com>
2020-05-20 14:45:15 +0530

    Removed the parameterization of the port as K8s was rejecting the image

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (cluster.isMaster) {
 } else {
     const express = require("express");
     const app = express();
-    const port = process.env.ECHO_PORT || 9595
+    const port = 9595
 
     var server = app.listen(port, () => {
         console.log("Server running on port " + port);
