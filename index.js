defdef2faa7e2f9dcfac0b4193116a7f057f68d0
Deeksha Deepak <deekshad1502@gmail.com>
2025-05-09 10:37:03 +0530

    SBCOSS-403: Added GitHub Actions to Sunbird Echo Service

--- index.js
+++ index.js
@@ -2,43 +2,43 @@ const cluster = require('cluster');
 
 if (cluster.isMaster) {
 
-    // Parallelism can be configured through env params
-    var cpuCount = process.env.PARALLELISM || 2;
+  // Parallelism can be configured through env params
+  var cpuCount = process.env.PARALLELISM || 2;
 
-    // Create a worker for each CPU
-    for (var i = 0; i < cpuCount; i += 1) {
-        cluster.fork();
-    }
+  // Create a worker for each CPU
+  for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
 } else {
-    const express = require("express");
-    const app = express();
-    const port = 9595
+  const express = require('express');
+  const app = express();
+  const port = 9595;
 
-    var server = app.listen(port, () => {
-        console.log("Server running on port " + port);
-    });
+  var server = app.listen(port, () => {
+    console.log('Server running on port ' + port);
+  });
 
-    server.on('connection', function(socket) {
-        //Below line has been added to check how many new connections are being made. This should be very few in a keep alive env
-        console.log("New connection was made by client");
-        socket.setTimeout(180 * 1000); 
-    });
+  server.on('connection', function(socket) {
+    //Below line has been added to check how many new connections are being made. This should be very few in a keep alive env
+    console.log('New connection was made by client');
+    socket.setTimeout(180 * 1000); 
+  });
 
-    app.get("/", (req, res, next) => {
-        res.status(200).send('hello');
-    });
+  app.get('/', (req, res, next) => {
+    res.status(200).send('hello');
+  });
 
-    app.get("/:reqpath", (req, res, next) => {
-        let reqpath = req.params.reqpath;
-        res.status(200).send(reqpath);
-    });
+  app.get('/:reqpath', (req, res, next) => {
+    let reqpath = req.params.reqpath;
+    res.status(200).send(reqpath);
+  });
 
-    app.get("/service/health", (req, res, next) => {
-        res.status(200).send('{"healthy":true}');
-    });
+  app.get('/service/health', (req, res, next) => {
+    res.status(200).send('{"healthy":true}');
+  });
 
-    app.use(function (req,res,next){
-        res.status(200).send('Unknown Path');
-    });
+  app.use(function (req,res,next){
+    res.status(200).send('Unknown Path');
+  });
     
 }
